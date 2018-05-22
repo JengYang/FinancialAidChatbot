466f0621844a12c3b1c950b8ea9cef2ce28f973f
Jeng Yang <chanjy-wa15@student.tarc.edu.my>
2018-05-22 15:35:39 +0800

    Fix error

--- app.py
+++ app.py
@@ -45,3 +45,10 @@ def makeWebhookResult(req):
         #"contextOut": [],
         "source": "FinancialAidChatBot"
     }
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print ("Starting app on port %d" %(port))
+
+    app.run(debug=True, port=port, host='0.0.0.0')
