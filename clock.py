bc1dbbecc7e7c46203176f82593311c87eb00145
Jeng Yang <chanjy-wa15@student.tarc.edu.my>
2018-11-04 16:44:45 +0800

    test clock

--- clock.py
+++ clock.py
@@ -3,12 +3,12 @@ import subscription
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=2)
-def timed_job():
-    subscription.sendMsg()
+#@sched.scheduled_job('interval', minutes=2)
+#def timed_job():
+#    subscription.sendMsg()
 
-#@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
-#def scheduled_job():
-#    print('This job is run every weekday at 5pm.')
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=9)
+def scheduled_job():
+    subscription.sendMsg()
 
 sched.start()
