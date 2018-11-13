a6799f15aef96da0339c2a8feca6723a2c2245b8
Jeng Yang <chanjy-wa15@student.tarc.edu.my>
2018-11-13 21:02:23 +0800

    test sub

--- subscription.py
+++ subscription.py
@@ -20,7 +20,7 @@ def sendMsg():
             sub = firebase.retrieveSub(a)
             for s in sub:
                 msg = b.get('name')+" application ends today. Apply now." 
-        fbbotw.post_text_message(s.get('fbId'),msg,'RESPONSE',None)      
+                fbbotw.post_text_message(s.get('fbId'),msg,'RESPONSE',None)      
     #fbbotw.post_text_message('2160418613974674',"Hi,XD",'RESPONSE',None) 
 
 
