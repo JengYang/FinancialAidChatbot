c80a81a56c51dc7164dabdbe9145f80f31439171
Jeng Yang <chanjy-wa15@student.tarc.edu.my>
2018-11-13 22:10:52 +0800

    test sub

--- subscription.py
+++ subscription.py
@@ -1,4 +1,4 @@
-from fbbotw import fbbotw
+#from fbbotw import fbbotw
 from fa_firebase import firebaseCRUD
 import datetime
 #add new code for subscription in future
@@ -13,17 +13,17 @@ def sendMsg():
         
         if datetime.datetime.strptime(b.get('startDate'),"%Y-%m-%d").date() == present:
             sub = firebase.retrieveSub(a)
-            for s in sub:
+            for s in sub.values():
                 print(s)
                 #if s.get('status') == 'active': #if status is needed
                 msg = b.get('name')+" application starts today. Apply now."
-               ## fbbotw.post_text_message(s.get('fbId'),msg,'RESPONSE',None) 
+                fbbotw.post_text_message(s.get('fbId'),msg,'RESPONSE',None) 
         elif datetime.datetime.strptime(b.get('endDate'),"%Y-%m-%d").date() == present:
             sub = firebase.retrieveSub(a)
-            for s in sub:
+            for s in sub.values():
                 msg = b.get('name')+" application ends today. Apply now." 
-             ##   fbbotw.post_text_message(s.get('fbId'),msg,'RESPONSE',None)      
+                fbbotw.post_text_message(s.get('fbId'),msg,'RESPONSE',None)      
 #fbbotw.post_text_message('2160418613974674',"Hi,XD",'RESPONSE',None) 
 
 
-#sendMsg()
+
