18326512e35a5fcf5e4ac40b3c16dfa328c913f0
Jeng Yang <chanjy-wa15@student.tarc.edu.my>
2018-11-13 22:51:55 +0800

    test sub

--- subscription.py
+++ subscription.py
@@ -19,7 +19,7 @@ def sendMsg():
                 print(s.get('fbId'))
                 #if s.get('status') == 'active': #if status is needed
                 msg = b.get('name')+" application starts today. Apply now."
-                fbbotw.post_text_message(s.get('fbId'),msg,'RESPONSE',None) 
+                fbbotw.post_text_message('2160418613974674',msg,'RESPONSE',None) 
         elif datetime.datetime.strptime(b.get('endDate'),"%Y-%m-%d").date() == present:
             sub = firebase.retrieveSub(a)
             for s in sub.values():
