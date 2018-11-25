d9c02c9a89b9acbd025987088665323d9bdf6526
JengYang <33310712+JengYang@users.noreply.github.com>
2018-11-25 11:53:31 +0800

    Update clock.py

--- clock.py
+++ clock.py
@@ -3,7 +3,7 @@ import subscription
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='0-6', hour=14)
+@sched.scheduled_job('cron', day_of_week='0-6', hour=12)
 def scheduled_job():
     subscription.sendMsg()
 
